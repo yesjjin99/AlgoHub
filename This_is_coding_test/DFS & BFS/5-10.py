5ed02bcf88b7ad5fd457407bae0ba3072d6641be
yesjjin99 <yesjjin99@naver.com>
2024-10-09 16:11:59 +0900

    이코테, DFS/BFS

--- This_is_coding_test/DFS & BFS/5-10.py
+++ This_is_coding_test/DFS & BFS/5-10.py
@@ -1,26 +1,30 @@
+from collections import deque
+
+dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
+
+def bfs(x, y):
+    queue = deque([(x, y)])
+    ice[x][y] = 1
+
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            if 0 <= nx < n and 0 <= ny < m and ice[nx][ny] == 0:
+                queue.append((nx, ny))
+                ice[nx][ny] = 1
+
+
 n, m = map(int, input().split())
+ice = []
+for _ in range(n):
+    ice.append(list(map(int, input())))
 
-graph = []
+count = 0
 for i in range(n):
-  graph.append(list(map(int, input())))
+    for j in range(m):
+        if ice[i][j] == 0:
+            bfs(i, j)
+            count += 1
 
-def dfs(x, y):
-  # 범위 체크
-  if x <= -1 or x >= n or y <= -1 or y >= m:
-    return False
-  if graph[x][y] == 0:
-    graph[x][y] = 1
-    dfs(x-1, y)
-    dfs(x+1, y)
-    dfs(x, y-1)
-    dfs(x, y+1)
-    return True
-  return False
-
-result = 0
-for i in range(n):
-  for j in range(m):
-    if dfs(i, j) == True:
-      result += 1
-
-print(result)
+print(count)
--- This_is_coding_test/DFS & BFS/5-11.py
+++ This_is_coding_test/DFS & BFS/5-11.py
@@ -1,30 +1,23 @@
 from collections import deque
 
-n, m = map(int, input().split())
 
-graph = []
-for i in range(n):
-  graph.append(list(map(int, input())))
-
-# 이동 방향
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+dx, dy = [0, 0, -1, 1], [-1, 1, 0, 0]
 
 def bfs(x, y):
-  queue = deque()
-  queue.append((x, y))
-  while queue:
-    x, y = queue.popleft()
-    for i in range(4):
-      nx = x + dx[i]
-      ny = y + dy[i]
-      if nx < 0 or ny < 0 or nx >= n or ny >= m:
-        continue
-      if graph[nx][ny] == 0:
-        continue
-      if graph[nx][ny] == 1:
-        graph[nx][ny] = graph[x][y] + 1
-        queue.append((nx, ny))
-  return graph[n-1][m-1]
+    queue = deque([(x, y)])
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i],
+            if 0 <= nx < n and 0 <= ny < m and maze[nx][ny] == 1:
+                queue.append((nx, ny))
+                maze[nx][ny] = maze[x][y] + 1
+    return maze[n - 1][m - 1]
+
+
+n, m = map(int, input().split())
+maze = []
+for _ in range(n):
+    maze.append(list(map(int, input())))
 
 print(bfs(0, 0))

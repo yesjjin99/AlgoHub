ab5e4368bbd813b8e3ef9bfa54243f5384523589
yesjjin99 <yesjjin99@naver.com>
2024-10-12 00:51:51 +0900

    10/11: 프로그래머스 lv2, 구현/재귀

---
+++ Programmers/2020Kakao/2.py
@@ -0,0 +1,40 @@
+def check(p):
+    cnt = 0
+    for c in p:
+        if c == '(':
+            cnt += 1
+        else:
+            cnt -= 1
+        if cnt < 0:
+            break
+
+    return cnt == 0
+
+
+def divide(p):
+    cnt = 0
+    for i, c in enumerate(p):
+        if c == '(':
+            cnt += 1
+        else:
+            cnt -= 1
+        if cnt == 0:
+            return p[:i + 1], p[i + 1:]
+
+
+def solution(p):
+    if not p:
+        return p
+
+    u, v = divide(p)
+
+    if check(u):
+        return u + solution(v)
+    else:
+        temp = '(' + solution(v) + ')'
+        for s in u[1:-1]:
+            if s == '(':
+                temp += ')'
+            else:
+                temp += '('
+        return temp

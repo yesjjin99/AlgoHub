49fa2dacb0e123d8607c56e6040cc514f44c87a3
yesjjin99 <yesjjin99@naver.com>
2024-10-21 12:55:02 +0900

    이코테, 이진탐색

--- This_is_coding_test/BinarySearch/7-5.py
+++ This_is_coding_test/BinarySearch/7-5.py
@@ -1,24 +1,55 @@
-def binary_search(arr, target, start, end):
-  while start <= end:
-    mid = (start + end) // 2
-
-    if arr[mid] == target:
-      return True
-    elif arr[mid] > target:
-      end = mid - 1
-    else:
-      start = mid + 1
-  return False
+import sys
+input = sys.stdin.readline
 
 n = int(input())
-n_arr = list(map(int, input().split()))
-n_arr.sort() # 이진 탐색 위해 정렬
+arr = list(map(int, input().split()))
+arr.sort()
 m = int(input())
-m_arr = list(map(int, input().split()))
+x = list(map(int, input().split()))
 
-for i in range(len(m_arr)):
-  success = binary_search(n_arr, m_arr[i], 0, n - 1)
-  if success:
-    print("yes", end=" ")
-  else:
-    print("no", end=" ")
+def binary_search(target):
+    start, end = 0, n - 1
+    while start <= end:
+        mid = (start + end) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return None
+
+for i in x:
+    if binary_search(i) is not None:
+        print('yes', end=' ')
+    else:
+        print('no', end=' ')
+
+# ---
+
+n = int(input())
+arr = [0] * 1000001
+m = int(input())
+x = list(map(int, input().split()))
+
+for i in input().split():  # 계수 정렬
+    arr[int(i)] = 1
+
+for i in x:
+    if arr[i] == 1:
+        print('yes', end=' ')
+    else:
+        print('no', end=' ')
+
+# ---
+
+n = int(input())
+arr = set(map(int, input().split()))  # set 자료구조를 사용하여 중복 제거
+m = int(input())
+x = list(map(int, input().split()))
+
+for i in x:
+    if i in arr:
+        print('yes', end=' ')
+    else:
+        print('no', end=' ')
--- This_is_coding_test/BinarySearch/7-8.py
+++ This_is_coding_test/BinarySearch/7-8.py
@@ -1,21 +1,23 @@
-def binary_search(arr, target, lenth, start, end):
-  answer = 0
-  while start <= end:
-    mid = (start + end) // 2
-    sum = 0
-    for i in range(lenth):
-      if arr[i] > mid:
-        sum += (arr[i] - mid)
-    
-    if sum < target:
-      end = mid - 1
-    else: # 적어도 m만큼
-      answer = mid
-      start = mid + 1
-  return answer
-
 n, m = map(int, input().split())
 arr = list(map(int, input().split()))
 arr.sort()
-answer = binary_search(arr, m, n, 0, arr[n-1])
-print(answer)
+start, end = 0, arr[-1]
+
+result = 0
+# 탐색 범위가 크므로 이진탐색 사용!
+while start <= end:
+    mid = (start + end) // 2  # 절단기 높이 설정
+    total = 0
+    for i in arr:
+        if i > mid:
+            total += i - mid
+
+    if total == m:
+        result = mid
+        break
+    elif total < m:
+        end = mid - 1
+    else:
+        start = mid + 1
+
+print(result)

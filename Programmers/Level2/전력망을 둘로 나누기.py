5a4ef9ead1d8bd593b4b60b0c0c15f93c83b2aa2
yesjjin99 <yesjjin99@naver.com>
2024-11-20 12:34:51 +0900

    11/20: 프로그래머스 lv2, Union Find

--- Programmers/Level2/전력망을 둘로 나누기.py
+++ Programmers/Level2/전력망을 둘로 나누기.py
@@ -32,3 +32,37 @@ def solution(n, wires):
         result.append(abs(bfs(i, j) - bfs(j, i)))
 
     return min(result)
+
+# ---
+
+def find(parent, x):
+    if parent[x] < 0:
+        return x
+    parent[x] = find(parent, parent[x])
+    return parent[x]
+
+def union(parent, a, b):
+    a = find(parent, a)
+    b = find(parent, b)
+
+    if parent[a] < parent[b]:
+        parent[a] += parent[b]
+        parent[b] = a
+    else:
+        parent[b] += parent[a]
+        parent[a] = b
+
+def solution(n, wires):
+    answer = 1e9
+
+    for w in range(len(wires)):
+        parent = [-1] * (n + 1)
+
+        for a, b in wires[:w] + wires[w + 1:]:  # 간선을 하나씩 차례대로 제외해보면서, 그 상태로 union find
+            union(parent, a, b)
+
+        cnt1 = parent[find(parent, wires[w][0])]  # 제외한 간선 양 끝 점의 루트 노드 parent 값이 송전탑 개수
+        cnt2 = parent[find(parent, wires[w][1])]
+        answer = min(answer, abs(cnt1 - cnt2))
+
+    return answer

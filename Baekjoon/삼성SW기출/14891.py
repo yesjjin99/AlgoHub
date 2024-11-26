f25751ffd163d3dc86d7b6d09586295cebd52239
yesjjin99 <yesjjin99@naver.com>
2024-11-26 11:43:38 +0900

    11/26: 백준, 구현

---
+++ Baekjoon/삼성SW기출/14891.py
@@ -0,0 +1,36 @@
+from collections import deque
+
+wheels = [deque(map(int, input().rstrip())) for _ in range(4)]
+
+def rotate_left(n, d):
+    if n == 0:
+        return
+
+    if wheels[n][6] != wheels[n - 1][2]:
+        rotate_left(n - 1, -d)
+        wheels[n - 1].rotate(-d)
+
+
+def rotate_right(n, d):
+    if n == 3:
+        return
+
+    if wheels[n][2] != wheels[n + 1][6]:
+        rotate_right(n + 1, -d)
+        wheels[n + 1].rotate(-d)
+
+
+for _ in range(int(input())):
+    n, d = map(int, input().split())
+    need_rotate = [(n - 1, d)]
+
+    rotate_left(n - 1, d)  # 왼쪽으로 차례로 이동하며 확인
+    rotate_right(n - 1, d)  # 오른쪽으로 차례로 이동하며 확인
+
+    wheels[n - 1].rotate(d)
+
+answer = 0
+for i in range(4):
+    if wheels[i][0] == 1:
+        answer += 2 ** i
+print(answer)

732f7815d24b84c70cabdd9f6c19d013ffb10fb2
yesjjin99 <yesjjin99@naver.com>
2024-11-27 11:17:16 +0900

    11/27: 백준, DFS/브루트포스/백트래킹

--- Baekjoon/삼성SW기출/15686.py
+++ Baekjoon/삼성SW기출/15686.py
@@ -1,3 +1,41 @@
+n, m = map(int,input().split())
+home, chicken = [], []
+for i in range(n): # 집과 치킨의 좌표를 리스트에 넣어준다.
+    cities = list(map(int,input().split()))
+    for j in range(n):
+        if cities[j] == 1:
+            home.append((i,j))
+        elif cities[j] == 2:
+            chicken.append((i,j))
+
+visited = [False] * len(chicken)
+result = 1e9
+
+def dfs(idx,cnt):
+    global result
+
+    if cnt == m:
+        total = 0
+        for i, j in home: # 집 좌표에 대해 모든 치킨집과의 거리를 구함
+            distance = 1e9 # 거리를 큰 수로 정의
+            for x, (a, b) in enumerate(chicken):
+                if visited[x]:
+                    distance = min(distance, abs(i - a) + abs(j - b))  # 각 집의 치킨 거리(최소)
+            total += distance
+        result = min(total, result)  # 도시의 치킨 거리(최소)
+        return
+
+    for i in range(idx, len(chicken)):  # 백트래킹을 사용할 경우, 이미 지난 인덱스는 다시 방문하지 않도록 해야 함 -> 안 그러면 시간 초과 뜸!
+        if not visited[i]:
+            visited[i] = True
+            dfs(i + 1, cnt + 1)
+            visited[i] = False
+
+dfs(0,0)
+print(result)
+
+# ---
+
 import sys
 from itertools import combinations
 input = sys.stdin.readline

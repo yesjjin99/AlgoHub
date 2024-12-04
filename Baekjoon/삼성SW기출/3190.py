3785170959282cbccdb96f7e9749dde7739395d8
yesjjin99 <yesjjin99@naver.com>
2024-12-04 10:48:44 +0900

    12/4: 백준, 구현

--- Baekjoon/삼성SW기출/3190.py
+++ Baekjoon/삼성SW기출/3190.py
@@ -1,50 +1,40 @@
 from collections import deque
 
 n = int(input())
-k = int(input())
-board = [[0] * n for _ in range(n)]
-for _ in range(k):
-    i, j = map(int, input().split())
-    board[i - 1][j - 1] = 1
-l = int(input())
-turn = deque()
-for _ in range(l):
-    turn.append(input().split())
-
-d = [0, 1]
-body = [[0] * n for _ in range(n)]
+graph = [[0] * n for _ in range(n)]
+for _ in range(int(input())):
+    a, b = map(int, input().split())
+    graph[a - 1][b - 1] = 1  # 사과의 위치
+dir_change = deque([list(input().split()) for _ in range(int(input()))])
 time = 0
-queue = deque([(0, 0)])
-body[0][0] = 1
 
-while queue:
+snake = deque([(0, 0)])
+graph[0][0] = 2  # 뱀의 위치
+dir = (0, 1)  # 오른쪽 방향
+while True:
     time += 1
-    nx, ny = queue[-1][0] + d[0], queue[-1][1] + d[1]  # 한 칸 이동
-    if nx < 0 or nx >= n or ny < 0 or ny >= n or body[nx][ny] == 1:
+    nx, ny = snake[0][0] + dir[0], snake[0][1] + dir[1]
+    if nx < 0 or nx >= n or ny < 0 or ny >= n or graph[nx][ny] == 2:  # 벽이나 자기자신의 몸과 부딪히면 게임 끝
         break
 
-    queue.append((nx, ny))
-    body[nx][ny] = 1
-    if board[nx][ny] == 1:  # 사과가 있다면
-        board[nx][ny] = 0
-    else:  # 사과가 없다면
-        a, b = queue.popleft()  # 꼬리 비우기
-        body[a][b] = 0
+    snake.appendleft((nx, ny))  # 몸길이를 늘려 머리를 다음칸에 위치
+    if graph[nx][ny] == 0:  # 이동한 칸에 사과가 없다면
+        a, b = snake.pop()  # 꼬리가 위치한 칸을 비워준다
+        graph[a][b] = 0
+    graph[nx][ny] = 2
 
-    if turn and int(turn[0][0]) == time:  # 방향 전환
-        _, c = turn.popleft()
-        if d == [0, 1]:
-            d = [-1, 0]
-        elif d == [0, -1]:
-            d = [1, 0]
-        elif d == [-1, 0]:
-            d = [0, -1]
-        elif d == [1, 0]:
-            d = [0, 1]
-
-        if c == 'D':
-            d[0] *= -1
-            d[1] *= -1
-
+    if dir_change and time == int(dir_change[0][0]):  # 방향 변환
+        x, y = dir
+        if dir_change[0][1] == 'L':
+            if x == 0:
+                dir = (-y, x)
+            elif y == 0:
+                dir = (y, x)
+        elif dir_change[0][1] == 'D':
+            if x == 0:
+                dir = (y, x)
+            elif y == 0:
+                dir = (y, -x)
+        dir_change.popleft()
 
 print(time)

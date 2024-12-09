2ac4732e3a77238f66e77bac3a2ce50e74672fb0
yesjjin99 <yesjjin99@naver.com>
2024-12-09 18:12:59 +0900

    12/9: 백준, BFS/백트래킹

--- Baekjoon/삼성SW기출/14502.py
+++ Baekjoon/삼성SW기출/14502.py
@@ -1,43 +1,37 @@
 import copy
 from collections import deque
-import sys
-input = sys.stdin.readline
-
 
 n, m = map(int, input().split())
-result = 0
-maps = []
-for _ in range(n):
-    maps.append(list(map(int, input().split())))
+maps = [list(map(int, input().split())) for _ in range(n)]
+dx, dy = [0, 0, -1, 1], [-1, 1, 0, 0]
+answer = 0
 
-dx, dy = [0, 0, -1, 1], [-1, 1, 0, 0],
-
-def bfs():  # 바이러스 퍼트리고 안전영역 계산
-    global result
+def bfs():
+    global answer
     queue = deque()
     temp = copy.deepcopy(maps)  # 백트래킹을 위한 확인용 map
+
     for i in range(n):
         for j in range(m):
-            if temp[i][j] == 2:
+            if temp[i][j] == 2:  # 바이러스가 존재하는 칸
                 queue.append((i, j))
 
     while queue:
         x, y = queue.popleft()
         for i in range(4):
             nx, ny = x + dx[i], y + dy[i]
-            if nx < 0 or nx >= n or ny < 0 or ny >= m:
-                continue
-            if temp[nx][ny] == 0:
-                temp[nx][ny] = 2
+            if 0 <= nx < n and 0 <= ny < m and temp[nx][ny] == 0:
+                temp[nx][ny] = 2  # 바이러스 퍼뜨리기
                 queue.append((nx, ny))
 
-    count = 0
-    for t in temp:
-        count += t.count(0)
-    result = max(result, count)
+    result = 0
+    for x in range(n):
+        result += temp[x].count(0)
+    answer = max(answer, result)
 
-def make_wall(empty):  # 벽 세우기
-    if empty == 3:
+
+def make_walls(cnt):
+    if cnt == 3:
         bfs()  # 바이러스 퍼트리기
         return
 
@@ -45,9 +39,8 @@ def make_wall(empty):  # 벽 세우기
         for j in range(m):
             if maps[i][j] == 0:  # 빈 공간이라면 벽 세우기 가능
                 maps[i][j] = 1  # 벽 세우기
-                make_wall(empty + 1)  # 다시 다음 벽 세우러 가기
+                make_walls(cnt + 1)  # 다시 다음 벽 세우러 가기
                 maps[i][j] = 0  # 다시 벽 허물기 (백트래킹)
 
-
-make_wall(0)
-print(result)
+make_walls(0)
+print(answer)

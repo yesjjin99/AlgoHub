85a6cc23ad80dca8024b5e30707d3fff7a9f3b54
yesjjin99 <yesjjin99@naver.com>
2024-12-10 11:51:24 +0900

    12/10: 백준, BFS

--- Baekjoon/삼성SW기출/16234.py
+++ Baekjoon/삼성SW기출/16234.py
@@ -1,47 +1,45 @@
 from collections import deque
 
+n, l, r = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
 dx, dy = [0, 0, -1, 1], [-1, 1, 0, 0]
 
 def bfs(x, y, visited):
-    total = land[x][y]
-    country = [(x, y)]
-    visited[x][y] = 1
     queue = deque([(x, y)])
+    visited[x][y] = 1
+    union = [(x, y)]
+    total = graph[x][y]
 
-    while queue:
+    while queue:  # 연합 생성
         x, y = queue.popleft()
         for i in range(4):
             nx, ny = x + dx[i], y + dy[i]
-            if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny]:
-                if l <= abs(land[nx][ny] - land[x][y]) <= r:  # 절대값!
+            if 0 <= nx < n and 0 <= ny < n and visited[nx][ny] == 0:
+                if l <= abs(graph[nx][ny] - graph[x][y]) <= r:
                     queue.append((nx, ny))
-                    country.append((nx, ny))
-                    total += land[nx][ny]
                     visited[nx][ny] = 1
+                    union.append((nx, ny))
+                    total += graph[nx][ny]
 
-    if len(country) <= 1:
+    if len(union) <= 1:
         return 0
-    now = total // len(country)
-    for a, b in country:
-        land[a][b] = now
+    total //= len(union)
+    for x, y in union:  # 인구수 갱신
+        graph[x][y] = total
+
     return 1
 
-
-land = []
-n, l, r = map(int, input().split())
-for _ in range(n):
-    land.append(list(map(int, input().split())))
-
-answer = 0
+result = 0
 while True:
-    temp = 0
+    check = 0
     visited = [[0] * n for _ in range(n)]
     for i in range(n):
         for j in range(n):
-            if not visited[i][j]:
-                temp += bfs(i, j, visited)
-    if temp == 0:
-        break
-    answer += 1
+            if visited[i][j] == 0:
+                check += bfs(i, j, visited)
 
-print(answer)
+    if check == 0:  # 더 이상 인구 이동이 발생하지 않는다면 종료
+        break
+    result += 1
+
+print(result)

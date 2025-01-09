510ffe0b8b1e60c33510895d7a568a56d2c191cf
yesjjin99 <yesjjin99@naver.com>
2025-01-09 12:46:02 +0900

    1/9: 백준, BFS/시뮬레이션

--- Baekjoon/삼성SW기출/14503.py
+++ Baekjoon/삼성SW기출/14503.py
@@ -2,36 +2,35 @@ from collections import deque
 
 n, m = map(int, input().split())
 r, c, d = map(int, input().split())
-room = [list(map(int, input().split())) for _ in range(n)]  # 0이면 빈 칸, 1이면 벽
+room = [list(map(int, input().split())) for _ in range(n)]  # 0은 빈 칸, 1은 벽
 
-dx, dy = [-1, 0, 1, 0], [0, 1, 0, -1]  # 북, 동, 남, 서
+dx, dy = [-1, 0, 1, 0], [0, 1, 0, -1]  # 0: 북, 1: 동, 2: 남, 3: 서
 
 def bfs(x, y, d):
-    q = deque([(x, y)])
-    room[x][y] = 2
-    count = 1
+    queue = deque([(x, y)])
+    room[x][y] = 2  # 청소된 상태
+    cnt = 1
 
-    while q:
-        x, y = q.popleft()
-        isEmpty = True
+    while queue:
+        x, y = queue.popleft()
+        empty = 0  # 청소되지 않은 빈 칸의 개수
+
         for _ in range(4):
-            d = (d + 3) % 4  # 회전
+            d = (d + 3) % 4  # 반시계 방향으로 90도 회전
             nx, ny = x + dx[d], y + dy[d]
 
-            if 0 <= nx < n and 0 <= ny < m and room[nx][ny] == 0:  # 청소되지 않은 빈 칸인 경우
-                q.append((nx, ny))
-                room[nx][ny] = 2  # 현재 칸 청소
-                count += 1
-                isEmpty = False
+            if 0 <= nx < n and 0 <= ny < m and room[nx][ny] == 0:
+                queue.append((nx, ny))
+                room[nx][ny] = 2
+                empty += 1
+                cnt += 1
                 break
 
-        if isEmpty:  # 현재 칸의 주변 4칸 중 청소되지 않은 빈 칸이 없다면
-            nx, ny = x - dx[d], y - dy[d]  # 후진
-            if nx < 0 or nx >= n or ny < 0 or ny >= m:
-                continue
-            if room[nx][ny] == 1:  # 벽이라면
-                return count
+        if empty == 0:  # 현재 칸의 주변 4칸 중 청소되지 않은 빈 칸이 없는 경우
+            nx, ny = x - dx[d], y - dy[d]  # 한 칸 후진
+            if room[nx][ny] == 1:  # 벽이라 후진할 수 없다면 작동을 멈춘다
+                return cnt
             else:
-                q.append((nx, ny))
+                queue.append((nx, ny))
 
 print(bfs(r, c, d))

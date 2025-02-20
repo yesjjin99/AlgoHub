11395894c298ddedf24c104f4893be622e345030
yesjjin99 <yesjjin99@naver.com>
2025-02-20 21:59:19 +0900

    2/20: 백준, BFS/시뮬레이션

--- Baekjoon/삼성SW기출/3190.py
+++ Baekjoon/삼성SW기출/3190.py
@@ -1,40 +1,50 @@
 from collections import deque
 
 n = int(input())
-graph = [[0] * n for _ in range(n)]
+board = [[0] * n for _ in range(n)]
+dc = []  # 뱀의 방향 변환
 for _ in range(int(input())):
     a, b = map(int, input().split())
-    graph[a - 1][b - 1] = 1  # 사과의 위치
-dir_change = deque([list(input().split()) for _ in range(int(input()))])
-time = 0
+    board[a - 1][b - 1] = 1  # 사과의 위치
+for _ in range(int(input())):
+    x, c = input().split()
+    dc.append([int(x), c])
 
-snake = deque([(0, 0)])
-graph[0][0] = 2  # 뱀의 위치
-dir = (0, 1)  # 오른쪽 방향
-while True:
-    time += 1
-    nx, ny = snake[0][0] + dir[0], snake[0][1] + dir[1]
-    if nx < 0 or nx >= n or ny < 0 or ny >= n or graph[nx][ny] == 2:  # 벽이나 자기자신의 몸과 부딪히면 게임 끝
-        break
+def dir_change(dx, dy, c):  # 방향 회전 함수
+    if dx == 0:
+        if c == 'L':  # 왼쪽으로 회전
+            return -dy, -dx
+        elif c == 'D':  # 오른쪽으로 회전
+            return dy, dx
+    elif dy == 0:
+        if c == 'L':
+            return dy, dx
+        elif c == 'D':
+            return -dy, -dx
 
-    snake.appendleft((nx, ny))  # 몸길이를 늘려 머리를 다음칸에 위치
-    if graph[nx][ny] == 0:  # 이동한 칸에 사과가 없다면
-        a, b = snake.pop()  # 꼬리가 위치한 칸을 비워준다
-        graph[a][b] = 0
-    graph[nx][ny] = 2
+def bfs(x, y):
+    queue = deque([(x, y)])
+    snake = deque([(x, y)])  # 현재 뱀이 위치해 있는 모든 칸
+    dx, dy = 0, 1  # 뱀은 처음에 오른쪽을 향한다
+    count = 0  # 시간
 
-    if dir_change and time == int(dir_change[0][0]):  # 방향 변환
-        x, y = dir
-        if dir_change[0][1] == 'L':
-            if x == 0:
-                dir = (-y, x)
-            elif y == 0:
-                dir = (y, x)
-        elif dir_change[0][1] == 'D':
-            if x == 0:
-                dir = (y, x)
-            elif y == 0:
-                dir = (y, -x)
-        dir_change.popleft()
+    while queue:
+        count += 1
+        x, y = queue.popleft()
+        nx, ny = x + dx, y + dy  # 이동
+        if nx < 0 or nx >= n or ny < 0 or ny >= n or (nx, ny) in snake:
+            return count  # 만약 벽이나 자기자신의 몸과 부딪히면 게임이 끝난다
 
-print(time)
+        queue.append((nx, ny))
+        snake.append((nx, ny))
+
+        if board[nx][ny] == 1:  # 만약 이동한 칸에 사과가 있다면, 그 칸에 있던 사과가 없어지고 꼬리는 움직이지 않는다
+            board[nx][ny] = 0
+        else:
+            snake.popleft()  # 꼬리가 위치한 칸 비워주기
+
+        if dc and dc[0][0] == count:
+            dx, dy = dir_change(dx, dy, dc[0][1])  # 방향 변환
+            dc.pop(0)
+
+print(bfs(0, 0))

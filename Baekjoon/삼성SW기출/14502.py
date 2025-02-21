4b21dd3ea60ffd3853b49e3e124a476aee2f3e4d
yesjjin99 <yesjjin99@naver.com>
2025-02-22 01:11:03 +0900

    2/22: 백준, BFS/DFS/백트래킹

--- Baekjoon/삼성SW기출/14502.py
+++ Baekjoon/삼성SW기출/14502.py
@@ -2,45 +2,46 @@ import copy
 from collections import deque
 
 n, m = map(int, input().split())
-maps = [list(map(int, input().split())) for _ in range(n)]
-dx, dy = [0, 0, -1, 1], [-1, 1, 0, 0]
+graph, virus = [], []  # 지도, 바이러스 위치
+dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
 answer = 0
+for i in range(n):
+    tmp = list(map(int, input().split()))
+    graph.append(tmp)
+    for j in range(m):
+        if tmp[j] == 2:
+            virus.append((i, j))
 
-def bfs():
-    global answer
-    queue = deque()
-    temp = copy.deepcopy(maps)  # 백트래킹을 위한 확인용 map
+def make_walls(count):
+    if count == 3:
+        bfs()
+        return
 
     for i in range(n):
         for j in range(m):
-            if temp[i][j] == 2:  # 바이러스가 존재하는 칸
-                queue.append((i, j))
+            if graph[i][j] == 0:
+                graph[i][j] = 1  # 벽 설치
+                make_walls(count + 1)
+                graph[i][j] = 0
+
+def bfs():
+    global answer
+
+    queue = deque(virus)  # 큐에 모든 바이러스 위치 넣어놓기
+    maps = copy.deepcopy(graph)
 
     while queue:
         x, y = queue.popleft()
         for i in range(4):
             nx, ny = x + dx[i], y + dy[i]
-            if 0 <= nx < n and 0 <= ny < m and temp[nx][ny] == 0:
-                temp[nx][ny] = 2  # 바이러스 퍼뜨리기
+            if 0 <= nx < n and 0 <= ny < m and maps[nx][ny] == 0:  # 빈 칸인 경우
+                maps[nx][ny] = 2  # 바이러스 퍼뜨기리
                 queue.append((nx, ny))
 
     result = 0
-    for x in range(n):
-        result += temp[x].count(0)
-    answer = max(answer, result)
-
-
-def make_walls(cnt):
-    if cnt == 3:
-        bfs()  # 바이러스 퍼트리기
-        return
-
-    for i in range(n):
-        for j in range(m):
-            if maps[i][j] == 0:  # 빈 공간이라면 벽 세우기 가능
-                maps[i][j] = 1  # 벽 세우기
-                make_walls(cnt + 1)  # 다시 다음 벽 세우러 가기
-                maps[i][j] = 0  # 다시 벽 허물기 (백트래킹)
+    for tmp in maps:
+        result += tmp.count(0)  # 안전 영역 크기 구하기
+    answer = max(answer, result)  # 안전 영역의 최대 크기 갱신
 
 make_walls(0)
 print(answer)

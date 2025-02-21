bb1dbeff84e48c8eec8b7dd1e5c55bc99746c5d6
yesjjin99 <yesjjin99@naver.com>
2025-02-21 23:34:26 +0900

    2/21: 백준, DFS/백트래킹

--- Baekjoon/삼성SW기출/14500.py
+++ Baekjoon/삼성SW기출/14500.py
@@ -1,44 +1,42 @@
 n, m = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
-dx, dy = [0, 0, -1, 1], [-1, 1, 0, 0]
+dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
+answer = 0
 
-result = 0
-def dfs(x, y, total, count, visited):  # ㅗ 모양을 제외한 나머지 모양 탐색
-    global result
+def dfs(x, y, count, total, visited):  # ㅗ 모양을 제외한 테트로미노 탐색
+    global answer
 
     if count == 4:
-        result = max(result, total)
+        answer = max(answer, total)
         return
 
     for i in range(4):
         nx, ny = x + dx[i], y + dy[i]
         if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny]:
             visited[nx][ny] = 1
-            dfs(nx, ny, total + graph[nx][ny], count + 1, visited)
+            dfs(nx, ny, count + 1, total + graph[nx][ny], visited)
             visited[nx][ny] = 0
 
-def find(x, y):  # ㅗ 모양 탐색 (DFS로 나올 수 없는 모양임)
-    global result
+def find(x, y):  # ㅗ 모양 테트로미노 탐색 (DFS 로 나올 수 없는 모양)
+    global answer
 
     arr = []
-    for i in range(4):
+    for i in range(4):  # ㅗ 모양을 만들기 위해 4방향 모두 추가
         nx, ny = x + dx[i], y + dy[i]
         if 0 <= nx < n and 0 <= ny < m:
             arr.append(graph[nx][ny])
 
     if len(arr) == 4:  # 만약 4방향 모두 추가된다면, 그 중 가장 작은 값 제거 후 sum
-        arr.sort(reverse=True)
-        arr.pop()
-        result = max(result, sum(arr) + graph[x][y])
+        answer = max(answer, graph[x][y] + sum(arr) - min(arr))
     elif len(arr) == 3:  # 만약 3방향만 추가된다면, 바로 sum
-        result = max(result, sum(arr) + graph[x][y])
-
+        answer = max(answer, graph[x][y] + sum(arr))
 
 visited = [[0] * m for _ in range(n)]
 for i in range(n):
-    for j in range(m):
+    for j in range(m):  # 모든 칸을 시작점으로 탐색
         visited[i][j] = 1
-        dfs(i, j, graph[i][j], 1, visited)
+        dfs(i, j, 1, graph[i][j], visited)
         find(i, j)
         visited[i][j] = 0
-print(result)
+
+print(answer)
